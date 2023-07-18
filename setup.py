b4b23476dcd80c4d7f6c29d2d00552c1e68ec1b9
Carey Metcalfe <carey@cmetcalfe.ca>
2023-07-17 21:47:48 -0400

    Update required Python version
    This mimics the minimum version required by discord.py v2
    
    Fixes #26

--- setup.py
+++ setup.py
@@ -20,14 +20,13 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pR0Ps/slack-to-discord",
     license="GPLv3",
-    python_requires=">=3.6.0",
+    python_requires=">=3.8.0",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
         "Topic :: Communications :: Chat",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
